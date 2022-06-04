8292bf5c3e76ce55ae118f0a50a8fa32bfd4f238
berkaynayman <berkaynayman4@gmail.com>
2022-06-05 01:15:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn){
+        this.title = title
+        this.author = author
+        this.isbn = isbn
+    }
+}
+
+// UI Class: Handle UI Tasks
+class UI{
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                autor: 'Berkay N.',
+                isbn: '6161'
+            },
+            {
+                title: 'Bordo Mavi',
+                author: 'Temel Dursun',
+                isbn: '1234'
+            }
+        ]
+        
+        const books = StoredBooks
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.getElementById('book-list')
+
+        const row = document.createElement('tr')
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger delete">X</a></td>
+        `
+
+        list.appendChild(row)
+    }
+
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
