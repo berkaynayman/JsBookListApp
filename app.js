372820cbca30d3a2904f13ea31e907cd1e114255
berkaynayman <berkaynayman4@gmail.com>
2022-06-06 00:24:04 +0300

    Add UI.clearFields(), Event Add Book

--- app.js
+++ app.js
@@ -43,9 +43,36 @@ class UI{
         list.appendChild(row)
     }
 
+    static clearFields() {
+        document.getElementById('title').value = ''
+        document.getElementById('author').value = ''
+        document.getElementById('isbn').value = ''
+    }
 }
 
 // Store Class: Handles Storage
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
+
+// Event: Add a Book
+document.getElementById('book-form')
+.addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault()
+
+    // Get form values
+    const title  = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const isbn   = document.getElementById('isbn').value
+
+    // Instatiate Book
+    const book = new Book(title, author, isbn)
+
+    // Add Book to UI
+    UI.addBookToList(book)
+
+    // Clear fields
+    UI.clearFields()
+})
